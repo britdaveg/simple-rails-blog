fe1ec5ae4f727e002b62d8647453ff5c47d3c3af
britdaveg <dave@performantsoftware.com>
2015-08-11 11:11:14 -0400

    Add new comments, incomplete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
 
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
@@ -10,6 +12,18 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @comment.destroy
+    redirect_to request.referrer || root_url
+  end
+
+  protected
+
+  def set_comment
+    @comment = Comment.find( params[:id] )
+  end
+
+  def comment_params
+    params.require(:comment).permit(:title, :content)
   end
 
 end
